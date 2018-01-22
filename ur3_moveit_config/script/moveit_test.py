f05147c71e68e41f54a819cdd78141f554f78400
hironoriyh <hironoriyh@gmail.com>
2018-01-22 18:19:29 +0900

    vel scaling factor works but not accel

--- ur3_moveit_config/script/moveit_test.py
+++ ur3_moveit_config/script/moveit_test.py
@@ -42,6 +42,9 @@ def move_group_python_interface_tutorial():
   print robot.get_current_state()
   print "============"
 
+  group.set_max_velocity_scaling_factor(0.01) 
+#   group.set_max_acceleration_scaling_factor(0.1)
+
   ## Planning to a Pose goal
   print "============ Generating plan 1"
   group.clear_pose_targets()
@@ -86,46 +89,6 @@ def move_group_python_interface_tutorial():
   group.go(wait=True)
   group.execute(plan1)
 
-  ## Cartesian Paths
-  ## ^^^^^^^^^^^^^^^
-  waypoints = []
-  current = group.get_current_pose().pose
-  waypoints.append(current)
-
-  print "waypoints"
-  print waypoints
-
-  # first orient gripper and move forward (+x)
-  wpose = geometry_msgs.msg.Pose()
-  wpose.position.x = waypoints[0].position.x + 0.1
-  waypoints.append(copy.deepcopy(wpose))
-
-  # second move down
-#   wpose.position.z -= 0.0
-#   waypoints.append(copy.deepcopy(wpose))
-
-  # third move to the side
-#   wpose.position.y += 0.05
-  waypoints.append(copy.deepcopy(wpose))
-
-  ## disabling it.
-  (plan3, fraction) = group.compute_cartesian_path(
-                               waypoints,   # waypoints to follow
-                               0.01,        # eef_step
-                               0.0)         # jump_threshold
-
-  print "============ Waiting while RVIZ displays plan3..."
-  rospy.sleep(3)
-  print "============ Visualizing plan3"
-  display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-  display_trajectory.trajectory_start = robot.get_current_state()
-  display_trajectory.trajectory.append(plan3)
-  display_trajectory_publisher.publish(display_trajectory);
-  rospy.sleep(2)
-  group.go(wait=True)
-  group.execute(plan3)
-
-#   collision_object = moveit_msgs.msg.CollisionObject()
 
 
   moveit_commander.roscpp_shutdown()
