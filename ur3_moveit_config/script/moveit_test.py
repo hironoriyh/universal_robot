83242a46732a11ae3bc7ac18e334898d8141cc80
hironoriyh <hironoriyh@gmail.com>
2018-01-23 19:19:39 +0900

    joint values working

--- ur3_moveit_config/script/moveit_test.py
+++ ur3_moveit_config/script/moveit_test.py
@@ -41,51 +41,38 @@ def move_group_python_interface_tutorial():
     print "============ Printing robot state"
     print robot.get_current_state()
     print "============"
-    group.set_max_velocity_scaling_factor(0.1)
+    group.set_max_velocity_scaling_factor(0.05)
     #   group.set_max_acceleration_scaling_factor(0.1)
 
     # Planning to a Pose goal
     print "============ Generating plan 1"
     group.clear_pose_targets()
-    group_variable_values = group.get_current_joint_values()
-    print "============ Joint values: ", group_variable_values
-    # group_variable_values = [-5.64, -1.60, -1.81, 3.42, -0.90, -3.13]
-    # group_variable_values = [-5.646939400826589, -1.6089308897601526, -1.8123844305621546, 3.42496919631958, -0.8998850027667444, -3.1375396887408655]
-    group_variable_values[0] = -5.7
-    group.set_joint_value_target(group_variable_values)
+    group.set_start_state_to_current_state()
+    pose_target = group.get_current_pose().pose
+    pose_target.position.z += 0.05
+    group.set_pose_target(pose_target)
     plan1 = group.plan()
-    rospy.sleep(1)
     group.go(wait=True)
+    rospy.sleep(3)
 
  ## Planning to a Pose goal
     print "============ Generating plan 2"
     group.clear_pose_targets()
-    group.set_start_state_to_current_state()
-    pose_target = geometry_msgs.msg.Pose()
-    pose_target.position.x = -0.32
-    pose_target.position.y = -0.03
-    pose_target.position.z = 0.25
-    pose_target.orientation.x = 0.0014905113208
-    pose_target.orientation.y = 0.0031527022559
-    pose_target.orientation.z =  0.999843349961
-    pose_target.orientation.w = 0.0173526477309
-    group.set_pose_target(pose_target)
+    group_variable_values = group.get_current_joint_values()
+    print "============ Joint values: ", group_variable_values
+    group_variable_values = [-5.64, -1.60, -1.61, 3.17, -0.90, -3.13]
+    # group_variable_values = [-5.646939400826589, -1.6089308897601526, -1.8123844305621546, 3.42496919631958, -0.8998850027667444, -3.1375396887408655]
+    # group_variable_values[0] = -5.7
+    group.set_joint_value_target(group_variable_values)
     plan1 = group.plan()
-    print "============ Waiting while RVIZ displays plan2..."
-    rospy.sleep(3)
-    print "============ Visualizing plan2"
-    display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-    display_trajectory.trajectory_start = robot.get_current_state()
-    display_trajectory.trajectory.append(plan1)
-    display_trajectory_publisher.publish(display_trajectory);
-    rospy.sleep(2)
     group.go(wait=True)
+    rospy.sleep(1)
 
     group.set_max_velocity_scaling_factor(0.005)
     rospy.sleep(3)
     print "============ Generating plan 3"
     pose_target = group.get_current_pose().pose
-    for i in range(3):
+    for i in range(1):
         group.clear_pose_targets()
         group.set_start_state_to_current_state()
         pose_target.position.x += 0.01
