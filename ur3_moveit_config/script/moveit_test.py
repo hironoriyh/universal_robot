0282f333ae614935c72eac59d19fbbd05f69ce20
hironoriyh <hironoriyh@gmail.com>
2018-01-26 22:05:55 +0900

    class

--- ur3_moveit_config/script/moveit_test.py
+++ ur3_moveit_config/script/moveit_test.py
@@ -10,7 +10,10 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+
 ## END_SUB_TUTORIAL
+
+import numpy as np
 
 from std_msgs.msg import String
 
@@ -20,7 +23,7 @@ def move_group_python_interface_tutorial():
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
     ## the robot as a whole.
-    robot = moveit_commander.RobotCommander()
+    robot =
     scene = moveit_commander.PlanningSceneInterface()
     group = moveit_commander.MoveGroupCommander("manipulator")
     display_trajectory_publisher = rospy.Publisher(
@@ -45,72 +48,52 @@ def move_group_python_interface_tutorial():
     #   group.set_max_acceleration_scaling_factor(0.1)
 
     # Planning to a Pose goal
-    print "============ Generating plan 1"
-    group.clear_pose_targets()
-    group.set_start_state_to_current_state()
-    pose_target = group.get_current_pose().pose
-    pose_target.position.z += 0.05
-    group.set_pose_target(pose_target)
-    plan1 = group.plan()
-    group.go(wait=True)
-    rospy.sleep(3)
-
- ## Planning to a Pose goal
-    print "============ Generating plan 2"
-    group.clear_pose_targets()
-    group_variable_values = group.get_current_joint_values()
-    print "============ Joint values: ", group_variable_values
-    group_variable_values = [-5.64, -1.60, -1.61, 3.17, -0.90, -3.13]
-    # group_variable_values = [-5.646939400826589, -1.6089308897601526, -1.8123844305621546, 3.42496919631958, -0.8998850027667444, -3.1375396887408655]
-    # group_variable_values[0] = -5.7
-    group.set_joint_value_target(group_variable_values)
-    plan1 = group.plan()
-    group.go(wait=True)
-    rospy.sleep(1)
-
-    group.set_max_velocity_scaling_factor(0.005)
-    rospy.sleep(3)
-    print "============ Generating plan 3"
-    pose_target = group.get_current_pose().pose
-    for i in range(1):
-        group.clear_pose_targets()
-        group.set_start_state_to_current_state()
-        pose_target.position.x += 0.01
-        print pose_target.position.x
-        group.set_pose_target(pose_target)
-        plan1 = group.plan()
-        group.go(wait=True)
-
-        rospy.sleep(0.5)
-        group.clear_pose_targets()
-        group.set_start_state_to_current_state()
-        pose_target.position.y -= 0.002
-        print pose_target.position.y
-        group.set_pose_target(pose_target)
-        plan1 = group.plan()
-        group.go(wait=True)
-
-        group.clear_pose_targets()
-        group.set_start_state_to_current_state()
-        pose_target.position.x -= 0.01
-        print pose_target.position.x
-        group.set_pose_target(pose_target)
-        plan1 = group.plan()
-        group.go(wait=True)
-
-        rospy.sleep(0.5)
-        group.clear_pose_targets()
-        group.set_start_state_to_current_state()
-        pose_target.position.y -= 0.002
-        print pose_target.position.y
-        group.set_pose_target(pose_target)
-        plan1 = group.plan()
-        group.go(wait=True)
-
+    print "============ Going up"
+    moveRelativePt([0.0, 0, 0.2], 0.05)
+    print "============ Side cut 1"
+    side_cut_1 =  np.loadtxt('1_first_sidecut_N.txt')
+    for pt in side_cut_1:
+        moveRelativePt(pt)
 
     moveit_commander.roscpp_shutdown()
     print "============ STOPPING"
+class MoveMilling:
+    def __init__(self, move_group, robot, scene):
+        self.robot = moveit_commander.RobotCommander()
+        self.scene = moveit_commander.PlanningSceneInterface()
+        self.group = moveit_commander.MoveGroupCommander("manipulator")
 
+    def moveRelativePt(self, pt, speed):
+        if(speed):
+            self.group.set_max_velocity_scaling_factor(0.005)
+
+        self.group.clear_pose_targets()
+        self.group.set_start_state_to_current_state()
+        pose_target = self.group.get_current_pose().pose
+
+        pose_target.position.x += pt[0]
+        pose_target.position.y += pt[1]
+        pose_target.position.z += pt[2]
+
+        self.group.set_pose_target(pose_target)
+        plan1 = self.group.plan()
+        self.group.go(wait=True)
+
+    def moveAbsPt(self, pt, speed):
+        if(speed):
+            self.group.set_max_velocity_scaling_factor(speed)
+
+        self.group.clear_pose_targets()
+        self.group.set_start_state_to_current_state()
+        pose_target = self.group.get_current_pose().pose
+
+        pose_target.position.x = pt[0]
+        pose_target.position.y = pt[1]
+        pose_target.position.z = pt[2]
+
+        self.group.set_pose_target(pose_target)
+        plan1 = self.group.plan()
+        self.group.go(wait=True)
 
 if __name__=='__main__':
   try:
